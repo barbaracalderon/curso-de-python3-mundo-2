5880d00af4f34974036fb5e6908f4a57199f7f0a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-09 12:17:22 -0300

    Retirar Fórmula Geral da PA
    Reescritura do código sem usar a fórmula geral da Progressão Aritmética.

--- desafio061.py
+++ desafio061.py
@@ -4,8 +4,9 @@ print('=========== PROGRESSÃO ARITMÉTICA ===========')
 primeiro = int(input('Digite o primeiro termo: '))
 r = int(input('Digite a razão dessa PA: '))
 termo = 0
-n = 1
-while n < 11:
-    termo = primeiro + (n - 1) * r #Fórmula Geral da PA
-    n += 1
-    print(termo, end=' ')
+contador = 1
+while contador <= 10:
+    print('{}, '.format(termo), end='')
+    termo = termo + r # Alternativa: termo += r
+    contador = contador + 1
+print('FIM')
